7005e48528afa4196ebc3c3bcae6f8a4f218c022
Giovanni Bellorio <gbellorio95@gmail.com>
2023-03-23 23:35:18 +0100

    arduino_client

--- coding/arduino_finale/arduino_finale.ino
+++ coding/arduino_finale/arduino_finale.ino
@@ -5,6 +5,12 @@ char ssid[] = "STIMATE_ROUTER";       //  your network SSID (name)
 char pass[] = "123456ff";             // your network password
 int status  = WL_IDLE_STATUS;         // the Wifi radio's status
 
+#define HOST "192.168.0.151"
+#define PORT 5050
+int connected;
+String data;
+WiFiClient client;
+
 // Pin per tutti gli input
 #define PWMA 5
 #define AIN2 4
@@ -17,7 +23,7 @@ int status  = WL_IDLE_STATUS;         // the Wifi radio's status
 // queste costanti vengono utilizzate per consentire di configurare i
 // motori e allinea con nomi di funzioni come forward.
 // Il valore può essere 1 o -1
-const int offsetA = 1;
+const int offsetA =  1;
 const int offsetB = -1;
 
 // Inizializzazione dei motori.
@@ -43,19 +49,69 @@ void setup() {
     Serial.println("Connected to network");
     Serial.println(WiFi.localIP());
   }
-
-  pinMode(AIN1, OUTPUT);
-  pinMode(AIN2, OUTPUT);
-  pinMode(BIN1, OUTPUT);
-  pinMode(BIN2, OUTPUT);
-  pinMode(PWMA, OUTPUT);
-  pinMode(PWMB, OUTPUT);
-  pinMode(STBY, OUTPUT);
 }
 
 void loop() {
-  //Motori avanti
-  forward(motor1, motor2, 100);
-  delay(1000);
   
+  if (connected == 0) {
+    if (client.connect(HOST, PORT)) {
+      Serial.println("# Connected to server #");
+      Serial.print("IP: ");
+      Serial.print(HOST);
+      Serial.print(", PORT: ");
+      Serial.println(PORT);
+  
+      connected = 1;
+    }
+  } 
+  else {
+    data = receiveData();
+    //Serial.println("Data: " + data);
+
+    if (data == "app_1_arduino"){
+      Serial.println("Start - NaoCar");
+      pinMode(STBY, OUTPUT);
+      fn_forward();
+      delay(2000);
+      brake(motor1, motor2);
+      pinMode(STBY, INPUT);
+      Serial.println("End - NaoCar");
+      sendData("arduino_1_nao");
+    }
+    // AGGIUNGERE GLI ALTRI CASI
+    
+  
+    if (client.connected()) {
+      String promptString = Serial.readString();
+      if (promptString != "") {
+        sendData(promptString);
+      }
+    } 
+    else {
+      Serial.println("No server available disconnecting");
+      client.stop();
+      connected = 0;
+    }
+  }
+  delay(50);
+}
+
+void fn_forward(){
+  forward(motor1, motor2, 100); //255
+}
+
+void sendData(String data) {
+  client.println(data);
+  Serial.print("Sent data: ");
+  Serial.println(data); 
+}
+
+String receiveData() {
+  if (client.available()) {
+    String dataFromServer = client.readStringUntil('\r');
+    Serial.print("Received data: ");
+    Serial.println(dataFromServer);
+    return dataFromServer;
+  }
+  return "";
 }
